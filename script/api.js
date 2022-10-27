cde29892ce67f32eec851cedb4dc2511b0aa5fc4
Kent Erik Hole <kent.erik.hole@outlook.com>
2022-10-27 15:01:14 +0200

    Update api.js
    Update the script

--- script/api.js
+++ script/api.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const url = 'https://rainydays.frontendkenterik.no/wp-json/';
+
+async function api(url) {
+  const response = await fetch(url);
+  const result = await response.json();
+
+  console.log(result);
+}
+
+api(url);
